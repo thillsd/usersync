1787efbcddb1ce3d1b93fdf69f2c9526446746e8
thillsd <111368948+thillsd@users.noreply.github.com>
2023-05-28 07:18:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Approved User Syncer
+====================
+
+Reddit bot to sync a userlist to the Reddit approved users for a subreddit.
+
+**Note:** There is a separate, undocumented ratelimit on adding approved users. This seems to be 100/30 minutes. The bot will automatically sleep for an hour when the ratelimit has been hit.
+
+
+## Installing
+
+Install python 3.8+:
+
+    apt install python3 python3-pip
+
+Install python dependancies:
+
+    pip install -U deserialize praw requests dotenv loguru
+
+
+## Configuration
+
+Create a `.env` file in the project root directory.
+
+Include:
+
+    username=""
+    password=""
+    client_id=""
+    client_secret=""
+    subreddit=""
+    user_agent="u/hillsd user comparer"
+    authorized_users_endpoint="https://raw.githubusercontent.com/EthTrader/donut.distribution/main/docs/users.json"
+
+
+## Running
+
+Perhaps set a cron job to run every 5 days. EG:
+
+    * * */5 * * /usr/bin/python3 /root/usersync/usersync.py >/dev/null 2>&1
---
+++ usersync.py
@@ -0,0 +1,127 @@
+import sys
+import time
+from typing import Dict, List, Optional
+
+import deserialize
+import praw
+import requests
+from dotenv import dotenv_values
+from loguru import logger
+from praw.exceptions import RedditAPIException
+from prawcore.exceptions import NotFound
+
+logger.remove(None)
+logger.add(
+    sys.stderr, format="<green>{time:HH:mm}" "</green> - <level>{message}</level>"
+)
+logger.add("run.log")
+
+
+@deserialize.parser("username", str.lower)
+class AuthorizedUser:
+    username: str
+    address: str
+    contrib: str
+    donut: str
+    weight: str
+
+
+def get_user_list(url: str) -> List[AuthorizedUser]:
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+
+    data = deserialize.deserialize(List[AuthorizedUser], response.json())
+    if data:
+        return data
+    else:
+        raise ValueError("Could not deserialize json")
+
+
+def get_existing_approved(r: praw.Reddit, subreddit: str) -> List[str]:
+    return list(
+        contributor.name.lower()
+        for contributor in r.subreddit(subreddit).contributor(limit=None)
+    )
+
+
+def user_exists(r: praw.Reddit, name: str):
+    try:
+        r.redditor(name).id
+    except (NotFound, AttributeError):
+        # NotFound => does not exist, AttributeError => banned
+        return False
+    return True
+
+
+def update_reddit(config: Dict) -> Optional[bool]:
+    website_users = [
+        user.username for user in get_user_list(config["authorized_users_endpoint"])
+    ]
+    logger.debug(f"Got userlist with {len(website_users)} users.")
+
+    r = praw.Reddit(
+        client_id=config["client_id"],
+        client_secret=config["client_secret"],
+        user_agent=config["user_agent"],
+        username=config["username"],
+        password=config["password"],
+    )
+
+    existing_reddit_approved_users = get_existing_approved(r, config["subreddit"])
+    logger.debug(
+        f"Got {len(existing_reddit_approved_users)} existing Reddit contributors."
+    )
+
+    successfully_added = 0
+    for user in website_users:
+        if user in existing_reddit_approved_users:
+            logger.debug(
+                f"Skipping website user {user!r} as they are already approved."
+            )
+            continue
+
+        if user_exists(r, user):
+            successfully_added += 1
+            logger.debug(
+                f"Adding website user {user!r} to approved list. [{successfully_added}]"
+            )
+            r.subreddit(config["subreddit"]).contributor.add(user)
+
+    for user in existing_reddit_approved_users:
+        if user not in website_users:
+            logger.debug(f"Deleting  user {user!r} from approved list.")
+            r.subreddit(config["subreddit"]).contributor.remove(user)
+
+    return True
+
+
+@logger.catch
+def main(config: Dict) -> None:
+    SIXTY_MINUTES = 60 * 60
+
+    while True:
+        try:
+            success = update_reddit(config)
+        except RedditAPIException as e:
+            if e.items[0].error_type == "SUBREDDIT_RATELIMIT":
+                logger.error(
+                    "Hit the separate hardcoded ratelimit of 100 users. Stopping for now"
+                )
+            else:
+                raise e
+            success = False
+
+        if success:
+            logger.debug("Completed job. Exiting.")
+
+        logger.error("Sleeping for an hour")
+        time.sleep(SIXTY_MINUTES)
+
+
+if __name__ == "__main__":
+    config = dotenv_values(".env")
+    if not config:
+        logger.error("No config file detected.")
+        logger.error("Quitting!")
+    else:
+        main(config)
